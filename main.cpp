87aa94c4deb1b110f4e7844a69500a3dbf4ca30e
Kathy Kanemoto <5721632+katkanemoto@users.noreply.github.com>
2025-02-25 19:41:25 +0000

    changed code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,75 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include "StateClimate.h"
+//#include "StateClimate.h"
+
+class StateClimate {
+    private:
+        int fips;
+        int year;
+        double temp;
+        double tempc;
+    
+    public:
+        // Constructor
+        StateClimate(int f, int y, double t, double tc);
+    
+        // Getters
+        int getFips() const;
+        int getYear() const;
+        double getTemp() const;
+        double getTempC() const;
+    
+        // Setters
+        void setFips(int f);
+        void setYear(int y);
+        void setTemp(double t);
+        void setTempC(double tc);
+    
+        // Display function
+        void display() const;
+};
+
+StateClimate::StateClimate(int f, int y, double t, double tc) : fips(f), year(y), temp(t), tempc(tc) {}
+
+// Getters
+int StateClimate::getFips() const { return fips; }
+int StateClimate::getYear() const { return year; }
+double StateClimate::getTemp() const { return temp; }
+double StateClimate::getTempC() const { return tempc; }
+
+// Setters
+void StateClimate::setFips(int f) { fips = f; }
+void StateClimate::setYear(int y) { year = y; }
+void StateClimate::setTemp(double t) { temp = t; }
+void StateClimate::setTempC(double tc) { tempc = tc; }
+
+// Display function
+void StateClimate::display() const {
+    std::cout << "FIPS: " << fips << ", Year: " << year 
+              << ", Temp (F): " << temp << ", Temp (C): " << tempc << std::endl;
+}
+
+//non-recursive binary search
+int binarySearch(const std::vector<StateClimate> &data, int targetFips) {
+    int low = 0;
+    int high = data.size() - 1;
+    int mid;
+
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+        if (data[mid].getFips() == targetFips) {
+            return mid;
+        } else if (data[mid].getFips() < targetFips) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
 
 int main() {
     std::vector<StateClimate> climateData;
